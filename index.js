3b3d1bdeefcc688b6edeb912ec589a7b04a33c25
Cleyton Duda Macedo <maccleyton@gmail.com>
2025-01-21 14:58:37 -0400

    update-21/01/25

--- index.js
+++ index.js
@@ -23,5 +23,22 @@ botaoAdicionar.addEventListener("click", (evento) => {
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+    
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
+        weekday: "long"});
+
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        hour: "numeric",
+        minute: "numeric"
+    })
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+
+    
     listaDeCompras.appendChild(itemDaLista);
+
 })
